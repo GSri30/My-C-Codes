b986457dd35b330db9bc02096c10466b360fcc11
G Sri Harsha <54752266+GSri30@users.noreply.github.com>
2019-10-05 12:56:33 +0530

    Add files via upload

---
+++ gcd.c
@@ -0,0 +1,44 @@
+// Code to find the GCD of two given numbers
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,i,A,B,gcd;
+    scanf("%d" "%d",&A,&B);
+    a=abs(A);
+    b=abs(B);
+
+if (a==0 && b==0)
+{
+    printf("Invalid input");
+}
+else if (a==0 && b!=0)
+{
+    printf("%d\n",b);
+}
+else if (a!=0 && b==0)
+{
+    printf("%d\n",a);
+}
+else if (a%b==0)
+{
+    printf("%d\n", b);
+
+}
+else
+{
+    for (i=1;i<=a && i<=b;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {   gcd=i;
+        }
+    }
+    printf("%d\n", gcd);
+}
+return 0;
+}
+
+
