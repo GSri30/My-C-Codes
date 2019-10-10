ed259b5de1e6739e0ebfea1fdca5717fcb18f06d
G Sri Harsha <54752266+GSri30@users.noreply.github.com>
2019-10-10 18:51:35 +0530

    Add files via upload

---
+++ finding_largest_num.c
@@ -0,0 +1,68 @@
+// code to find the largest of four given numbers
+
+
+#include<stdio.h>
+
+int max(int a,int b,int c,int d)
+{
+
+	if(a > b)
+	{
+        	if(a > c)
+		{
+            		if(a>d)
+	    		{
+			    	return a;
+	    		}
+            		else 
+	    		{
+				return d;
+	    		}
+        	}
+		else
+       		{
+          	  	if(c > d)
+	    		{
+			    	return c;
+	    		}
+            		else
+	    		{
+				return d;
+			}
+        	}
+	}
+	else
+	{
+		if(b > c)
+		{
+			if(b > d)
+			{
+				return b;
+			}
+            		else 
+	    		{
+				return d;
+			}
+        	}
+        	else
+		{
+			if(c > d)
+			{
+				return c;
+			}
+            		else 
+			{
+				return d;
+			}
+        	}
+	}
+}
+  
+
+int main()
+{
+	int a,b,c,d;
+	scanf("%d %d %d %d", &a,&b,&c,&d);
+	printf("%d",max(a,b,c,d));
+	return 0;
+}
